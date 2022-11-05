972b814b0969ed093280df83e26be0f229e27d30
alessandro-giuzio <giuzio@icloud.com>
2022-11-05 12:50:38 +0100

    navbar from DasPro

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,20 +1,10 @@
-{{-- <header class="banner"> --}}
- {{--  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a> --}}
-  {{-- @include('partials.page-header') --}}
-{{--   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif --}}
-{{-- </header> --}}
-
-
-    <div class="flex items-center justify-between gap-4 bg-bg1 py-4 px-[120px]">
-        {{-- logo --}}
-        <div>
-          <svg width="135" height="48" viewBox="0 0 135 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<header class="bg-bg3 md:top-0 z-50 px-4 xl:px-0  overflow-x-hidden">
+    <div
+      class="w-full max-w-6.5xl flex md:justify-between items-center mx-auto py-4"
+    >
+      <!-- Logo Container -->
+      <div class="flex items-center p-0 gap-4">
+        <svg width="135" height="48" viewBox="0 0 135 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_19_469)">
             <path d="M63.4298 40.5085C61.3114 40.5085 59.3254 40.3431 57.4719 40.012C55.6514 39.7141 54.0793 39.2673 52.7553 38.6715V31.8696C54.2116 32.4985 55.817 33.0115 57.5713 33.4087C59.3586 33.8059 61.0467 34.0045 62.6354 34.0045C64.6214 34.0045 66.0943 33.8224 67.0542 33.4584C68.0472 33.0943 68.5437 32.2999 68.5437 31.0753C68.5437 30.2477 68.2954 29.5857 67.7989 29.0892C67.3356 28.5927 66.5577 28.1624 65.4654 27.7984C64.4062 27.4012 62.9498 26.9378 61.0963 26.4082C58.9117 25.7463 57.174 25.018 55.8832 24.2237C54.5923 23.3961 53.6654 22.4031 53.1028 21.2447C52.5401 20.0863 52.2588 18.6629 52.2588 16.9749C52.2588 13.9298 53.3511 11.5963 55.5357 9.97442C57.7201 8.35251 60.9473 7.54159 65.2172 7.54159C67.0707 7.54159 68.8747 7.69054 70.6289 7.98843C72.3831 8.25322 73.8064 8.56767 74.8987 8.93173V15.7833C73.4754 15.2207 72.0356 14.8069 70.5793 14.5421C69.1559 14.2774 67.7658 14.1449 66.4088 14.1449C64.6214 14.1449 63.1651 14.3104 62.0396 14.6414C60.9473 14.9725 60.4012 15.7337 60.4012 16.9253C60.4012 17.6203 60.5998 18.183 60.997 18.6134C61.3942 19.0105 62.0728 19.3745 63.0326 19.7056C64.0256 20.0366 65.3662 20.4338 67.0542 20.8972C69.669 21.5923 71.6715 22.4363 73.0617 23.4292C74.4519 24.3891 75.4117 25.5145 75.9413 26.8054C76.471 28.0631 76.7358 29.4865 76.7358 31.0753C76.7358 33.8887 75.6435 36.1726 73.4589 37.9267C71.2743 39.648 67.9314 40.5085 63.4298 40.5085Z" fill="#DBDBD7"/>
             <path d="M88.9873 40.012V28.2452L77.5186 8.03807H85.8596L93.0092 21.2943L100.159 8.03807H108.5L97.0302 28.2452V40.012H88.9873Z" fill="#DBDBD7"/>
@@ -24,14 +14,148 @@
 
             </svg>
       </div>
-        {{-- navigation --}}
-        <div class="flex gap-4 items-center justify-end">
-          <a href="#" class="text-base text-txPri font-leading-10 text-center tracking-[0.2px]">Home</a>
-          <a href="#" class="text-base text-txPri font-leading-10 text-center tracking-[0.2px]">Blog</a>
-          @include('components.button' , ['text' => 'Termin vereinbaren'])
+      <!-- Nav Container -->
+      <nav
+        class="bottom-0 left-0 bg-neutral-50 md:bg-transparent px-4 w-full z-50 fixed md:top-0 md:left-0 md:relative flex items-center justify-center md:justify-end gap-4 text-xs md:text-base grow"
+      >
+        <a
+          class="basis-1/5 md:basis-auto text-base text-bg3 md:text-txPri font-normal leading-10   flex flex-col py-2 md:py-0 md:inline  lg:px-4 text-center"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="md:hidden mx-auto"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+          </svg>
+          Home
+        </a>
+        <a
+          class="basis-1/5 md:basis-auto text-base text-bg3 md:text-txPri font-normal leading-10   flex flex-col py-2 md:py-0 md:inline  lg:px-4 text-center"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="md:hidden mx-auto"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <rect x="4" y="4" width="6" height="6" rx="1"></rect>
+            <rect x="4" y="14" width="6" height="6" rx="1"></rect>
+            <rect x="14" y="14" width="6" height="6" rx="1"></rect>
+            <line x1="14" y1="7" x2="20" y2="7"></line>
+            <line x1="17" y1="4" x2="17" y2="10"></line>
+          </svg>
+          Blog
+        </a>
+        {{-- <a
+          class="basis-1/5 md:basis-auto text-base text-txPri font-normal leading-10   flex flex-col py-2 md:py-0 md:inline  lg:px-4 text-center"
+          href="https://aptex.gumroad.com/l/ztmepz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="md:hidden mx-auto"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx="9" cy="7" r="4"></circle>
+            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+            <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
+          </svg>
+          About us
+        </a>
+        <a
+          class="basis-1/5 md:basis-auto text-base text-txPri font-normal leading-10   flex flex-col py-2 md:py-0 md:inline  lg:px-4 text-center"
+          href="https://aptex.gumroad.com/l/ztmepz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="md:hidden mx-auto"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11"
+            ></path>
+            <line x1="8" y1="8" x2="12" y2="8"></line>
+            <line x1="8" y1="12" x2="12" y2="12"></line>
+            <line x1="8" y1="16" x2="12" y2="16"></line>
+          </svg>
+          Blog
+        </a>
+        <a
+          class="basis-1/5 md:basis-auto md:text-neutral-0 flex flex-col py-2 md:py-0 md:inline md:leading-10 lg:px-4 text-center"
+          href="https://aptex.gumroad.com/l/ztmepz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="md:hidden mx-auto"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M10 21v-6.5a3.5 3.5 0 0 0 -7 0v6.5h18v-6a4 4 0 0 0 -4 -4h-10.5"
+            ></path>
+            <path d="M12 11v-8h4l2 2l-2 2h-4"></path>
+            <path d="M6 15h1"></path>
+          </svg>
+          Contact
+        </a> --}}
+      </nav>
+      <!-- CTA Container -->
+
+      <div class="hidden md:flex items-center gap-2 shrink-0">
+        @include('components.button' , ['text' => 'Termin vereinbaren'])
       </div>
     </div>
-
-
-
-
+  </header>
